f62c757cf63718649b9e004a69624f10300e6956
William F. Dudley Jr. <37313619+wfdudley@users.noreply.github.com>
2025-02-07 10:52:48 -0500

    Added all_segments()
    Added all_segments(), also cleaned up some comments.
    Corrected 16th character of seq6 to be truly "cleared".
    Made SPI speed a #define to remove "magic numbers" from code.

--- M66005.cpp
+++ M66005.cpp
@@ -2,8 +2,9 @@
 #include <SPI.h>
 #include "M66005.h"
 
-/* by george9801
+/* originally by george9801
  * heavily re-written and fixed by W.F. Dudley Jr. 2025 01 09
+ * more mods by W.F. Dudley Jr. 2025 02 06
  */
 
 /*  text by george9801:
@@ -20,8 +21,8 @@
  *  The 66005 part comes up in 66004 mode after reset
  */
 
-// if your hardware was designed by a psychopath, with character 0 at the
-// RIGHT end, then enable PSYCHOPATH_MODE
+// if your hardware was designed by a native Japanese speaker, with
+// character 0 at the RIGHT end, then enable RT_TO_LEFT_MODE
 #define RT_TO_LEFT_MODE 1
 // my display uses position 15 for all the icons, so my first character
 // location is position 14, and I can write 15 chars max.  If this is
@@ -31,34 +32,23 @@
 #define BEGIN_TR_DELAY 1
 #define INTRA_CHAR_DELAY 15
 #define END_TR_DELAY 5
+#define SPI_SPEED 400000
 
 #if M68005_MODE
 static const uint8_t seq1[] = { 1, 0xfe };	// set to M68005 mode
 static const uint8_t seq2[] = { 2, 0x10, 0x07 }; // set length 16 digits
 static const uint8_t seq3[] = { 2, 0x11, 0x07 }; // set brightness to 14/16
 static const uint8_t seq4[] = { 1, 0xf1 };	// set "all digit display" to "normal"
-static const uint8_t seq4[] = { 1, 0xf5 };	// auto-increment char position
+static const uint8_t seq5[] = { 1, 0xf5 };	// auto-increment char position
+static const uint8_t seq6[] = { 16, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xa0 };	// write blanks (clear) display
 #else	// M68004 mode
 static const uint8_t seq1[] = { 1, 0xfd };	// set to M68004 mode (redundant)
 static const uint8_t seq2[] = { 1, 0x07 };	// set length 16 digits
 static const uint8_t seq3[] = { 1, 0x0f };	// set brightness to 14/16
 static const uint8_t seq4[] = { 1, 0xf1 };	// set "all digit display" to "normal"
 static const uint8_t seq5[] = { 1, 0xf5 };	// auto-increment char position
-static const uint8_t seq6[] = { 16, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xa5 };	// write blanks (clear) display
+static const uint8_t seq6[] = { 16, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xa0 };	// write blanks (clear) display
 #endif
-
-/* icons:
- 0x21 ! A, Dolby, Neo:6, Vol, RDS
- 0x2d - A, B, PCM, TUNED, MUTING
- 0x2e . HD, MSTR, FM STEREO, RDS
- 0x2f / A, Dolby, DTS, Audyssey, AUTO
- 0x60 ` M.opt, Neo:6, AUTO
- 0x5f _ Exp, FM STEREO, RDS, SLEEP, dB
- 0xa2   A, Dolby, PLII x, Neo:6, Vol   
- 0xa3   A, D, MSTR, Exp, FM STEREO, RDS
- 0xa4   AAC, HD, RDS
- 0xa5   A, Dolby, D, MUTING
- */
 
 const uint8_t *init_sequences[] = {
 	seq1, seq2, seq3, seq4, seq5, seq6
@@ -75,7 +65,7 @@ unsigned char c;
     _pspi = pspi;
 #if 1
     for(uint16_t i = 0 ; i < sizeof(init_sequences)/sizeof(uint8_t *) ; i++) {
-	_pspi->beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+	_pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
 	digitalWrite(_fl_cs, LOW);	// CS pin low, enable data read
 	delayMicroseconds(BEGIN_TR_DELAY);
 	for(uint8_t j = 1 ; j <= init_sequences[i][0] ; j++) {
@@ -92,7 +82,7 @@ unsigned char c;
 
 void M66005::brightness(uint8_t brightness) {
 unsigned char c;
-    _pspi->beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+    _pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
     digitalWrite(_fl_cs, LOW);	// CS pin low, enable data read
     delayMicroseconds(BEGIN_TR_DELAY);
     c = 0xfd;	// set to M66004 mode
@@ -105,13 +95,26 @@ unsigned char c;
     _pspi->endTransaction();
 }
 
+// all off = 0, normal operation = 0x01, all on = 0x03
+void M66005::all_segments(uint8_t off_norm_on) {
+unsigned char c;
+    _pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
+    digitalWrite(_fl_cs, LOW);	// CS pin low, enable data read
+    delayMicroseconds(BEGIN_TR_DELAY);
+    c = 0xf0 | (off_norm_on & 0x03);
+    _pspi->transfer(c);
+    delayMicroseconds(INTRA_CHAR_DELAY);
+    digitalWrite(_fl_cs, HIGH);	// CS pin high, disable data read
+    _pspi->endTransaction();
+}
+
 // pos: 0 means "first" character in the display
 // character codes starting with 0000, 0001, 1000, 1110, or 1111 are
 // reserved for commands
 // "normal" ASCII is outside of these ranges (starts with 0010 through 0111)
 // in other words, character codes are (0x20 through 0x7f and 0xa0 through 0xdf)
 void M66005::write_string(const char *str, int len, int pos) {
-    _pspi->beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+    _pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
     digitalWrite(_fl_cs, LOW);
 #if RT_TO_LEFT_MODE
     pos = (15 + ICONS_OFFSET) - (len + pos);
@@ -141,7 +144,7 @@ void M66005::write_string(const char *str, int len, int pos) {
 
 void M66005::clear(void) {
 int pos;
-    _pspi->beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+    _pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
     digitalWrite(_fl_cs, LOW);
     pos = 0;
     delayMicroseconds(BEGIN_TR_DELAY);
@@ -163,7 +166,7 @@ int pos;
 }
 
 void M66005::define_char(int ramaddress, const char * fivebyseven) {
-    _pspi->beginTransaction(SPISettings(400000, MSBFIRST, SPI_MODE0));
+    _pspi->beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
     digitalWrite(_fl_cs, LOW);
     delayMicroseconds(BEGIN_TR_DELAY);
     _pspi->transfer(0xfc);	// write to ram
--- M66005.h
+++ M66005.h
@@ -4,6 +4,10 @@
 #include "Arduino.h"
 #include <SPI.h>
 
+#define ALL_SEGMENTS_OFF    0
+#define ALL_SEGMENTS_NORMAL 1
+#define ALL_SEGMENTS_ON     3
+
 class M66005 {
 
     private:
@@ -14,6 +18,7 @@ class M66005 {
 	M66005(uint8_t cs);
 	void init(SPIClass *pspi);
 	void brightness(uint8_t brightness);
+	void all_segments(uint8_t off_norm_on);
 	void write_string(const char *str, int len, int pos);
 	void clear(void);
 	void define_char(int ramaddress, const char * fivebyseven);
