d0ce0a9c87914c38f81eed3d2434d5d86611a8d1
William F. Dudley Jr. <37313619+wfdudley@users.noreply.github.com>
2025-04-24 17:18:29 -0400

    Update M66005.cpp
    Added Apache License boilerplate.

--- M66005.cpp
+++ M66005.cpp
@@ -1,7 +1,20 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include "M66005.h"
+/* Copyright 2025 William F. Dudley Jr.
 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 /* originally by george9801
  * heavily re-written and fixed by W.F. Dudley Jr. 2025 01 09
  * more mods by W.F. Dudley Jr. 2025 02 06
